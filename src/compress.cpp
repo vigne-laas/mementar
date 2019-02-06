5805796d8b278c84a590fd71ba9c058b2c3f5289
Guillaume Sarthou <gsarthou@laas.fr>
2019-02-06 11:44:56 +0100

    [ADD] arguments for compress executable

--- src/compress.cpp
+++ src/compress.cpp
@@ -12,7 +12,37 @@ using namespace std::chrono;
 
 int main (int argc, char* argv[])
 {
-  std::ifstream t("../tests_files/test.txt");
+  std::string input_file = "";
+  std::string output_file = "";
+
+  if (argc != 5)
+  {
+    std::cout << "not enough arguments " << argc << std::endl;
+    return -1;
+  }
+
+  if(std::string(argv[1]) == "-i")
+    input_file = std::string(argv[2]);
+  else if(std::string(argv[1]) == "-o")
+    output_file = std::string(argv[2]);
+  else
+  {
+    std::cout << "arguments must be -i or -o " << std::string(argv[1]) << std::endl;
+    return -1;
+  }
+
+  if(std::string(argv[3]) == "-i")
+    input_file = std::string(argv[4]);
+  else if(std::string(argv[3]) == "-o")
+    output_file = std::string(argv[4]);
+  else
+  {
+    std::cout << "arguments must be -i or -o" << std::string(argv[4]) << std::endl;
+    return -1;
+  }
+
+  std::ifstream t(input_file);
+  //std::ifstream t("../tests_files/syslog");
   //std::ifstream t("/home/gsarthou/Downloads/owl-export-unversioned.owl");
   std::string in((std::istreambuf_iterator<char>(t)),
                    std::istreambuf_iterator<char>());
@@ -23,7 +53,7 @@ int main (int argc, char* argv[])
   high_resolution_clock::time_point t1 = high_resolution_clock::now();
 
   LzCompress comp;
-	comp.compress(in, "../tests_files/out");
+	comp.compress(in, output_file);
 
   high_resolution_clock::time_point t2 = high_resolution_clock::now();
   duration<double> time_span = duration_cast<duration<double>>(t2 - t1);
