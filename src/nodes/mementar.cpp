afb1774b10011e9ada3d6e1e5729bc31587646ed
Guillaume Sarthou <gsarthou@laas.fr>
2019-02-21 17:19:59 +0100

    [ADD] IdManager

--- src/nodes/mementar.cpp
+++ src/nodes/mementar.cpp
@@ -1,6 +1,7 @@
 #include "ros/ros.h"
 
 #include "mementar/archiving_compressing/archiving/Header.h"
+#include "mementar/IdManager.h"
 
 int main(int argc, char** argv)
 {
@@ -10,8 +11,21 @@ int main(int argc, char** argv)
 
   ros::service::waitForService("ontologenius/rest", -1);
 
-  std::string intern_folder = std::string(argv[1]);
-  std::cout << "intern_folder " << intern_folder << std::endl;
+  /*std::string intern_folder = std::string(argv[1]);
+  std::cout << "intern_folder " << intern_folder << std::endl;*/
+
+  IdManager<uint32_t> ids;
+
+  std::cout << ids.getNewId() << std::endl; // 0
+  std::cout << ids.getNewId() << std::endl; // 1
+  std::cout << ids.getNewId() << std::endl; // 2
+  std::cout << ids.getNewId() << std::endl;
+  std::cout << ids.getNewId() << std::endl;
+  std::cout << ids.getNewId() << std::endl;
+  std::cout << ids.getNewId() << std::endl; // 6
+  ids.removeId(2);
+  std::cout << ids.getNewId() << std::endl; // 2
+  std::cout << ids.getNewId() << std::endl; // 7
 
   ros::spin();
 
