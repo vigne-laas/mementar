4ddad747a13dc934c614838d9422a76da456ba37
Guillaume Sarthou <gsarthou@laas.fr>
2019-06-20 18:51:33 +0200

    [UPDATE] clean mementar node

--- src/nodes/mementar.cpp
+++ src/nodes/mementar.cpp
@@ -1,7 +1,6 @@
 #include "ros/ros.h"
 
 #include "mementar/RosInterface.h"
-#include "mementar/core/IdManager.h"
 
 int main(int argc, char** argv)
 {
@@ -13,21 +12,8 @@ int main(int argc, char** argv)
   ros::NodeHandle n;
   mementar::RosInterface interface(&n, directory);
 
-  mementar::IdManager<uint32_t> ids;
-
-  std::cout << ids.getNewId() << std::endl; // 0
-  std::cout << ids.getNewId() << std::endl; // 1
-  std::cout << ids.getNewId() << std::endl; // 2
-  std::cout << ids.getNewId() << std::endl;
-  std::cout << ids.getNewId() << std::endl;
-  std::cout << ids.getNewId() << std::endl;
-  std::cout << ids.getNewId() << std::endl; // 6
-  ids.removeId(2);
-  std::cout << ids.getNewId() << std::endl; // 2
-  std::cout << ids.getNewId() << std::endl; // 7
-
-  ros::spin();
-
+  interface.run();
+  
   ROS_DEBUG("KILL mementar");
 
   return 0;
